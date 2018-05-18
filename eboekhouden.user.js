2c23979d9633c6586117c68bcaf6703356f7bbd9
Jayke Meijer <jayke@xomnia.com>
2018-05-18 17:16:30 +0200

    Added buttons to select today, this week (#2)

--- eboekhouden.user.js
+++ eboekhouden.user.js
@@ -6,14 +6,6 @@
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // ==/UserScript==
 
-/* Script by Jayke Meijer for e-boekhouden, grouping results by day
- * TODO:
- * - Add button to select results for current week
- * - Add functionality to easily hide all but today
- * - See if state can be remembered
- * - Make hidden notion more compact (hide header or change to a completely different row)
- * - Add button to show just the current week
- */
 
 function checkChanged() {
   frame = $(document.getElementById('mainframe').contentDocument);
@@ -43,11 +35,16 @@ function addStyle(head, css) {
 }
 
 function preparePage(frame) {
-  addStyle(frame.find('head').get(0), '.tROW {border-bottom: 2px solid #AAAAAA; font-weight: bold;} .hidebutton {font-family: "Lucida Console", Monaco, monospace} .hidebutton:hover {color: #BADA55; cursor: pointer}');  
+  addStyle(frame.find('head').get(0),
+           '.tROW {border-bottom: 2px solid #AAAAAA; font-weight: bold;} ' +
+           '.hidebutton {font-family: "Lucida Console", Monaco, monospace} ' +
+           '.hidebutton:hover {color: #BADA55; cursor: pointer}' +
+           '.clickable {text-decoration: underline;} .clickable:hover {color:#BADA55; cursor:pointer}');  
   table = frame.find('body > center > table');
- 
+
   if (checkPage(table)) {
-    runRedesign(table);
+    addCurrentButtons(frame);
+    setTimeout(function() {runRedesign(table);}, 200);
   } else {
     // Not overview page, ignore
   }
@@ -89,7 +86,9 @@ function runRedesign(table) {
 
   total_hours = 0.0;
   insertables = [];
+
   for(var i=1; i < rows.length; i++) {
+    console.log('Processing rows');
     d = rows[i].childNodes[2].innerHTML;
     t = parseFloat(rows[i].childNodes[6].innerHTML.replace(',', '.'));
 
@@ -111,7 +110,7 @@ function runRedesign(table) {
       total_hours += t;
     }
   }
-
+  
   /* Insert headers and total rows */
   for(var i = 0; i < insertables.length; i++) {
     if (i + 1 < insertables.length) {
@@ -141,4 +140,54 @@ function runRedesign(table) {
   }
 }
 
+function getDateString(d) {
+  return d.getDate() + '-' + (d.getMonth() + 1) + '-' + d.getFullYear();
+}
+
+function selectVrije() {
+  frm = frame.find('#frm');
+  frm.find('input').filter('[value=V]').get()[0].click();
+}
+
+function selectDateRange(from, to) {
+  selectVrije();
+  van = frm.find('#txtDatumVan').first();
+  tot = frm.find('#txtDatumTot').first();
+  van.get()[0].value = getDateString(from);
+  tot.get()[0].value = getDateString(to);
+  rb = frm.find('#submit1').get()[0].click();
+}
+
+function showToday() {
+  today = new Date();
+  today_s = getDateString(today);
+  selectDateRange(today, today);
+}
+
+function showThisWeek() {
+  function getMonday(d) {
+    d = new Date(d);
+    var day = d.getDay();
+    var diff = d.getDate() - day + (day == 0 ? -6:1);
+    return new Date(d.setDate(diff));
+  }
+
+  today = new Date();
+  monday = getMonday(today);
+  sunday = new Date(monday);
+  sunday.setDate(sunday.getDate() + 6);
+  
+	selectDateRange(monday, sunday);  
+}
+
+function addCurrentButtons(frame) {
+  frm = frame.find('#frm');
+  frm.children().first().find('table').first().find('tbody > tr').append(
+    '<td class="clickable" id="showtoday">Vandaag</td><td class="clickable" id="showthisweek">Deze week</td>'
+  );
+  frame.find('#showtoday').first().click(showToday);
+  frame.find('#showthisweek').first().click(showThisWeek);
+}
+
 setInterval(checkChanged, 100);
+
