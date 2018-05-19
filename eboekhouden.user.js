bb1a826ede3eda41f33e4b12ac1e856ac6a853be
Jayke Meijer <jayke.meijer@gmail.com>
2018-05-19 16:30:18 +0200

    Many improvements:
    - Added button to select current month
    - Added button to add hours for current day
    - Added buttons per day to add hours (#6)
    - Stability improvements (related to #7)

--- eboekhouden.user.js
+++ eboekhouden.user.js
@@ -6,48 +6,9 @@
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // ==/UserScript==
 
-
-function checkChanged() {
-  frame = $(document.getElementById('mainframe').contentDocument);
-  hidden = frame.find('#changedByJayke').length;
-  if (hidden > 0) {
-    // No change, ignore
-  } else {
-    element = $('<div id="changedByJayke"></div>');
-    element.css('display', 'none');
-    frame.find('body').append(element);
-    
-    preparePage(frame);
-  }
-}
-
-function checkPage(table) {
-  header = table.find('table > tbody > tr > td > font');
-  return header.text().indexOf('GEREGISTREERDE') > -1;
-}
-
-function addStyle(head, css) {
-  var style;
-  style = document.createElement('style');
-  style.type = 'text/css';
-  style.innerHTML = css;
-  head.appendChild(style);
-}
-
-function preparePage(frame) {
-  addStyle(frame.find('head').get(0),
-           '.tROW {border-bottom: 2px solid #AAAAAA; font-weight: bold;} ' +
-           '.hidebutton {font-family: "Lucida Console", Monaco, monospace} ' +
-           '.hidebutton:hover {color: #BADA55; cursor: pointer}' +
-           '.clickable {text-decoration: underline;} .clickable:hover {color:#BADA55; cursor:pointer}');  
-  table = frame.find('body > center > table');
-
-  if (checkPage(table)) {
-    addCurrentButtons(frame);
-    setTimeout(function() {runRedesign(table);}, 200);
-  } else {
-    // Not overview page, ignore
-  }
+/* ======= Shortcut getters ======= */
+function getFrame() {
+  return $(document.getElementById('mainframe').contentDocument);
 }
 
 function getTableBody(table) {
@@ -58,6 +19,195 @@ function getRows(table) {
   return table.children('tbody').children('tr').eq(2).find('table > tbody:contains("Datum") > tr');
 }
 
+
+/* ======= Checking if execution is required logic ======= */
+function checkChanged() {
+  frame = getFrame();
+  hidden = frame.get()[0].getElementById('changedByJayke');
+  if (hidden != null) {
+    /* No change, ignore */
+  } else {
+    /* Add detector used to detect frame change */
+    element = $('<div id="changedByJayke"></div>');
+    element.css('display', 'none');
+    frame.find('body').append(element);
+    
+    /* Add CSS */
+    addStyle(frame.find('head').get(0),
+           '.tROW {border-bottom: 2px solid #AAAAAA; font-weight: bold; padding-bottom: 20px;} ' +
+           '.hidebutton {font-family: "Lucida Console", Monaco, monospace} ' +
+           '.hidebutton:hover {color: #BADA55; cursor: pointer} ' +
+           '.add_hours:hover {color: #BADA55; cursor: pointer} ' +
+           '.clickable {text-decoration: underline;} ' +
+           '.clickable:hover {color:#BADA55; cursor:pointer} ' +
+           '#addForToday {margin-left: 10px;} ' + 
+           '#addForToday:hover {color:#BADA55; cursor:pointer}');
+    
+    /* Add our functionality to the page*/
+    waitForPage();
+  }
+}
+
+function addStyle(head, css) {
+  var style;
+  style = document.createElement('style');
+  style.type = 'text/css';
+  style.innerHTML = css;
+  head.appendChild(style);
+}
+
+/* Wait for page content to load, then trigger page rebuild */
+function waitForPage(frame) {
+  center = getFrame().find('body > center');
+  if (center.length == 0) {
+    setTimeout(waitForPage, 100);
+  } else {
+    buildPage(center);
+  }
+}
+
+function checkPageOverzicht(table) {
+  header = table.find('table > tbody > tr > td > font');
+  return header.text().indexOf('GEREGISTREERDE') > -1;
+}
+
+function checkPageToevoegen(form) {
+  header = form.find('center > table > tbody > tr > td').find('font');
+  return header.text().indexOf('TIJDREGISTRATIE') > -1;
+}
+
+function buildPage(center) {
+  table = center.children('table');
+  form = center.children('form');
+
+  if (checkPageOverzicht(table)) {
+    runRedesignOverzicht(table);
+  } else if (checkPageToevoegen(form)) {
+    // Check if date select in URL. If so, set it as date
+    url = new URL(getFrame().context.URL);
+    date = url.searchParams.get("SELECTDAY");
+    if (date != null) {
+      form.find('#txtDatum').first().get()[0].value = date;
+    }
+
+    // Force set to Investering (second option) by default
+    form.find('#SelActiviteit option').prop('selected', true).trigger('change');
+  } else {
+    // Not overview page, ignore
+  }
+}
+
+
+/* ======= Perform changes to 'Overzicht' page ======= */
+function runRedesignOverzicht(table) {
+  parent = getTableBody(table);
+  rows = getRows(table);
+  header = rows[0];
+
+  if (rows.length > 1) {
+    current_date = rows[1].childNodes[2].innerHTML;
+
+    total_hours = 0.0;
+    insertables = [];
+
+
+    for(var i=1; i < rows.length; i++) {
+      d = rows[i].childNodes[2].innerHTML;
+      t = parseFloat(rows[i].childNodes[6].innerHTML.replace(',', '.'));
+
+      if (d != current_date) {      
+        newRow = $('<tr style="line-height:21px; margin-bottom:5px;">' +
+                   '<td class="cROW tROW"></td>' +
+                   '<td class="cROW tROW add_hours" data-date="' + current_date +'" colspan=2>+ Uren toevoegen</td>' +
+                   '<td class="cROW tROW"></td>' +
+                   '<td class="cROW tROW"></td>' +
+                   '<td class="cROW tROW" style="text-align:right">Totaal ' + current_date + ':</td>' +
+                   '<td class="cROW tROW" style="text-align:right">' + total_hours.toFixed(2).replace('.', ',') + '</td>' +
+                   '</tr>');
+        insertables.push([i - 1, newRow]);
+
+        current_date = d;
+        total_hours = t;
+      } else {
+        total_hours += t;
+      }
+    }
+
+    /* Insert headers and total rows */
+    for(var i = 0; i < insertables.length; i++) {
+      if (i + 1 < insertables.length) {
+        $(rows[insertables[i][0]]).after($(header).clone());
+      }
+      $(rows[insertables[i][0]]).after(insertables[i][1]);
+    }
+
+    /* Add function to headers */
+    getFrame().find('.add_hours').click(addHoursToDay);
+  
+    /* Create collapsable days */
+    rows = getRows(table);
+    prev_header = 0;
+    for(var i=1; i < rows.length; i++) {
+      if (rows[i].childNodes[3].innerHTML.indexOf('<b>Datum</b>') > -1 || i == rows.length - 1) {
+        rows[prev_header].childNodes[1].classList.add('hidebutton');
+        rows[prev_header].childNodes[1].innerHTML = '[-]';
+
+        var a = prev_header + 1;
+        var b = i - 2;
+        rows[prev_header].childNodes[1].setAttribute("data-start", a);
+        rows[prev_header].childNodes[1].setAttribute("data-stop", b);
+        rows[prev_header].childNodes[1].onclick = function() {
+          toggleRows(this, rows);
+        }
+
+        prev_header = i;
+      }
+    }
+  }
+  
+  /* Add quick buttons to select current day, week and month, as well as adding hours for today */
+  frm = getFrame().find('#frm');
+  if (frm.find('.clickable').length == 0) {  // Hack to prevent double addition
+    addCurrentButtons();
+    addHoursCurrentDayButton();
+  }
+}
+
+function getDateString(d) {
+  return d.getDate() + '-' + (d.getMonth() + 1) + '-' + d.getFullYear();
+}
+
+function addCurrentButtons() {
+  frm = getFrame().find('#frm');
+  frm.children().first().find('table').first().find('tbody > tr').append(
+    '<td class="clickable" id="showtoday">Vandaag</td>' +
+    '<td class="clickable" id="showthisweek">Deze week</td>' +
+    '<td class="clickable" id="showthismonth">Deze maand</td>'
+  );
+  frame.find('#showtoday').first().click(showToday);
+  frame.find('#showthisweek').first().click(showThisWeek);
+  frame.find('#showthismonth').first().click(showThisMonth);
+}
+
+function addHoursCurrentDayButton() {
+  frm = getFrame().find('#frm').first();
+  sib = frm.next().next();
+  link = sib.get()[0].cloneNode();
+  link.setAttribute('id', 'addForToday');
+  link.setAttribute('href', link.href + '&SELECTDAY=' + getDateString(new Date()));
+  link.innerHTML = 'Toevoegen voor vandaag';
+  sib.after(link);
+}
+  
+function addHoursToDay(element) {
+  date = element.target.dataset.date;
+  link = getFrame().find('center').first().children('a').first().get()[0];
+  link.setAttribute('href', link.href + '&SELECTDAY=' + date);
+  link.click();
+}
+
+
+/* ======= User-triggered scripts ======= */
 function toggleRows(element, rows) {
   var start = element.dataset.start;
   var stop = element.dataset.stop;
@@ -75,87 +225,6 @@ function toggleRows(element, rows) {
       $(rows[i]).show();
     }
   }
-}
-
-function runRedesign(table) {
-  parent = getTableBody(table);
-  rows = getRows(table);
-  header = rows[0];
-
-  current_date = rows[1].childNodes[2].innerHTML;
-
-  total_hours = 0.0;
-  insertables = [];
-
-  for(var i=1; i < rows.length; i++) {
-    console.log('Processing rows');
-    d = rows[i].childNodes[2].innerHTML;
-    t = parseFloat(rows[i].childNodes[6].innerHTML.replace(',', '.'));
-
-    if (d != current_date) {      
-      newRow = $('<tr style="line-height:21px; margin-bottom:5px;">' +
-                 '<td class="cROW tROW"></td>' +
-                 '<td class="cROW tROW"></td>' +
-                 '<td class="cROW tROW"></td>' +
-                 '<td class="cROW tROW"></td>' +
-                 '<td class="cROW tROW"></td>' +
-                 '<td class="cROW tROW" style="text-align:right">Totaal ' + current_date + ':</td>' +
-                 '<td class="cROW tROW" style="text-align:right">' + total_hours.toFixed(2).replace('.', ',') + '</td>' +
-                 '</tr>');
-      insertables.push([i - 1, newRow]);
-
-      current_date = d;
-      total_hours = t;
-    } else {
-      total_hours += t;
-    }
-  }
-  
-  /* Insert headers and total rows */
-  for(var i = 0; i < insertables.length; i++) {
-    if (i + 1 < insertables.length) {
-      $(rows[insertables[i][0]]).after($(header).clone());
-    }
-    $(rows[insertables[i][0]]).after(insertables[i][1]);
-  }
-
-  /* Create collapsable days */
-  rows = getRows(table);
-  prev_header = 0;
-  for(var i=1; i < rows.length; i++) {
-    if (rows[i].childNodes[3].innerHTML.indexOf('<b>Datum</b>') > -1 || i == rows.length - 1) {
-      rows[prev_header].childNodes[1].classList.add('hidebutton');
-      rows[prev_header].childNodes[1].innerHTML = '[-]';
-
-      var a = prev_header + 1;
-      var b = i - 2;
-      rows[prev_header].childNodes[1].setAttribute("data-start", a);
-      rows[prev_header].childNodes[1].setAttribute("data-stop", b);
-      rows[prev_header].childNodes[1].onclick = function() {
-        toggleRows(this, rows);
-      }
-
-      prev_header = i;
-    }
-  }
-}
-
-function getDateString(d) {
-  return d.getDate() + '-' + (d.getMonth() + 1) + '-' + d.getFullYear();
-}
-
-function selectVrije() {
-  frm = frame.find('#frm');
-  frm.find('input').filter('[value=V]').get()[0].click();
-}
-
-function selectDateRange(from, to) {
-  selectVrije();
-  van = frm.find('#txtDatumVan').first();
-  tot = frm.find('#txtDatumTot').first();
-  van.get()[0].value = getDateString(from);
-  tot.get()[0].value = getDateString(to);
-  rb = frm.find('#submit1').get()[0].click();
 }
 
 function showToday() {
@@ -180,14 +249,27 @@ function showThisWeek() {
 	selectDateRange(monday, sunday);  
 }
 
-function addCurrentButtons(frame) {
-  frm = frame.find('#frm');
-  frm.children().first().find('table').first().find('tbody > tr').append(
-    '<td class="clickable" id="showtoday">Vandaag</td><td class="clickable" id="showthisweek">Deze week</td>'
-  );
-  frame.find('#showtoday').first().click(showToday);
-  frame.find('#showthisweek').first().click(showThisWeek);
+function showThisMonth() {
+  frm = getFrame().find('#frm');
+  frm.find('input').filter('[value=M]').get()[0].click();
+  rb = frm.find('#submit1').get()[0].click();
 }
 
-setInterval(checkChanged, 100);
+function selectVrije() {
+  frm = getFrame().find('#frm');
+  frm.find('input').filter('[value=V]').get()[0].click();
+}
 
+function selectDateRange(from, to) {
+  selectVrije();
+  van = frm.find('#txtDatumVan').first();
+  tot = frm.find('#txtDatumTot').first();
+  van.get()[0].value = getDateString(from);
+  tot.get()[0].value = getDateString(to);
+  rb = frm.find('#submit1').get()[0].click();
+}
+
+
+
+/* Start checking */
+setInterval(checkChanged, 100);
